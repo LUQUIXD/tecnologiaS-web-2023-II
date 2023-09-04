89ea0d34eed27c2ebfcc49fcc57160ea2d76eca3
unknown <LUQUIXD>
2023-09-04 15:19:31 -0500

    OPERADORES

---
+++ FUNDAMENTOS-php/op-asignacion.php
@@ -0,0 +1,22 @@
+<?php
+$numero=10;//ASIGNACIÓN
+echo $numero;
+echo "<br>";
+
+$numero +=5;//INCREMENTO
+print $numero;
+echo "<br>";
+
+$numero -=3;//DECREMENTO
+print $numero;
+echo "<br>";
+
+$numero *=2;//MULTIPLICAR
+print $numero;
+echo "<br>";
+
+$numero /=4;//INCREMENTO
+print $numero;
+echo "<br>";
+
+?>
---
+++ FUNDAMENTOS-php/operadores-aritmeticos.php
@@ -0,0 +1,24 @@
+<?php
+$numero=10;
+$numero2=50;
+
+$suma=$numero+$numero2;
+$resta=$numero2-$numero;
+$mult=$numero*$numero2;
+$div=$numero2/$numero;
+
+echo "SUMA: ".$suma;
+echo "<br>";
+
+
+echo "RESTA: ".$resta;
+echo "<br>";
+
+
+echo "MULTIPLICACIÓN: ".$mult;
+echo "<br>";
+
+
+echo "DIVISIÓN: ".$div;
+echo "<br>";
+?>
